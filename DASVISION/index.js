2f51aedd76cd8ad686d6badd3cca46475d608009
brayantheboss <bryancebaramirez@gmail.com>
2026-04-20 22:08:05 -0500

    Implement basic HTTP server in index.js

--- DASVISION/index.js
+++ DASVISION/index.js
@@ -1,1 +1,14 @@
-console.log("Hola Mundo desde EC2 con CodeDeploy!");
+const http = require('http');
+
+const server = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  res.end('Hola Mundo - DataVision funcionando en la nube!\n');
+});
+
+// Render usa el puerto que ellos te asignan o el 3000 por defecto
+const port = process.env.PORT || 3000;
+
+server.listen(port, () => {
+  console.log(`Servidor corriendo en el puerto ${port}`);
+});
